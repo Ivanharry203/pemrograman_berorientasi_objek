cca6c85de636121b86cb960580711cc0b1b557ef
Ivanharry203 <167224743+Ivanharry203@users.noreply.github.com>
2024-08-26 14:20:15 +0700

    Update Week_2_PBO.java

--- week2_pbo/Week2_PBO.java
+++ week2_pbo/Week2_PBO.java
@@ -11,6 +11,11 @@ package com.ivan_harry.week2_pbo;
 public class Week2_PBO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String[]items = {"Nama : Ivan Harry Faisya","NPM : 2320506048","Alamat : Geduang,Kalijoso,Secang,Magelang","No HP : 08913893719128"};
+        
+        for (String item : items) {
+            System.out.println(item);
+        }
+    
     }
 }
