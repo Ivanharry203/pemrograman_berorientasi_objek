3fdc5b41ecf6ec39d9dbe886c52d21ce6a11a775
ivanharry203 <ivanharry203@gmail.com>
2024-08-26 14:07:11 +0700

    week2_pbo

---
+++ week2_pbo/Week2_PBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ivan_harry.week2_pbo;
+
+/**
+ *
+ * @author acer
+ */
+public class Week2_PBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
