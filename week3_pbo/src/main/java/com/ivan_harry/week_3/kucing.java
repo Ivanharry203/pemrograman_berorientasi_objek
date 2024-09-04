9253d5b245c2c35cb9a00419bfcf6c2c78ad4d87
ivanharry203 <ivanharry203@gmail.com>
2024-09-04 14:14:50 +0700

    week_3
    week_3

---
+++ week3_pbo/src/main/java/com/ivan_harry/week_3/kucing.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ivan_harry.week_3;
+
+/**
+ *
+ * @author acer
+ */
+public class kucing {
+    String nama;
+    int umur;
+    void suara() {
+        System.out.println("Hewan Bersuara");
+    }
+    
+}
---
+++ week3_pbo/src/main/java/com/ivan_harry/week_3/panggil_kucing.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ivan_harry.week_3;
+
+/**
+ *
+ * @author acer
+ */
+public class panggil_kucing {
+    public static void main(String[] args){
+        kucing kucing = new kucing();
+        kucing.nama = "mimi";
+        kucing.umur = 3;
+        kucing.suara();
+    }
+    
+}
